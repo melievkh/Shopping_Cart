221b4651fc9bbc819d89837899bcebb1b0eb3e82
khusanmeliev <khusanmeliev@gmail.com>
2022-07-02 21:28:55 +0500

    added login/register page

---
+++ src/Pages/Login/Login.style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import colors from '../../assets/color/colors';
+import FlexBox from '../../components/Flexbox/FlexBox';
+
+export const Wrapper = styled(FlexBox)`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Box = styled.form`
+  width: 350px;
+  height: 380px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 4px;
+  input {
+    width: 80%;
+    height: 25px;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid ${colors.black};
+    border-radius: 8px;
+    text-indent: 8px;
+    &:focus{
+        border-bottom: 1px solid ${colors.blue};
+    }
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+    }
+  }
+`;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Button from '../../components/button/Button';
+import FlexBox from '../../components/Flexbox/FlexBox';
+import Heading from '../../components/Heading/Heading';
+import { Box, Wrapper } from './Register.style';
+import { FiUserPlus } from 'react-icons/fi';
+import colors from '../../assets/color/colors';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  const initialState = {
+    name: '',
+    phone_number: '',
+    password: '',
+    confirm_password: '',
+  };
+  const [details, setDetails] = useState(initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if (details.phone_number.length < 7) {
+    //   alert("Telefon raqamni to'g'ri kiriting");
+    //   return false;
+    // } else if (details.password !== details.confirm_password) {
+    //   alert("Parol noto'g'ri kiritildi!");
+    //   return false;
+    // }
+  };
+  return (
+    <Wrapper>
+      <Box
+        onSubmit={()=> handleSubmit(details)}
+        wd="350px"
+        hg="450px"
+        justifyContent="space-evenly"
+      >
+        <Heading color={colors.blue}>
+          <FiUserPlus /> Ro'yhatdan o'tish
+        </Heading>
+        <FlexBox wd="100%" gap="30px">
+          <input type="text" placeholder=" Ism" name="name" required />
+          <input
+            type="number"
+            placeholder="Telefon raqam"
+            name="phone_number"
+            required
+          />
+          <input type="password" placeholder="Parol" name="password" required />
+          <input
+            type="password"
+            placeholder="Parolni qaytaring"
+            name="confirm_password"
+            required
+          />
+        </FlexBox>
+        <Button wd="80%">Akkount yaratish</Button>
+        <Heading style={{ fontSize: '12px' }}>
+          Akkountgiz allaqachon mavjudmi? <Link to="/login">Kirish</Link>
+        </Heading>
+      </Box>
+    </Wrapper>
+  );
+};
+
+export default Register;
