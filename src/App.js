5446e7e1ba10b21df4e4a927df1ee0e8bb3e66e2
melievkh <khushnudmeliev21@gmail.com>
2022-07-04 16:28:44 +0500

    removed unnecessary pages and shortened foodlist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import Navbar from './containers/Navbar/Navbar';
-import DarkFood from './Pages/darkFood/DarkFood';
-import LiquidFood from './Pages/liquidFood/LiquidFood';
-import Salad from './Pages/Salad/Salad';
-import Drinks from './Pages/drinks/Drinks';
+import Products from './Pages/Products/Products';
 import { Route, Routes } from 'react-router-dom';
 import { Admin } from './components/Dashboard/Admin';
-import Register from './Pages/Register/Register'
+import Register from './Pages/Register/Register';
 import Login from './Pages/Login/Login';
 
 function Main() {
   return (
     <>
       <Navbar />
-      <LiquidFood />
-      <DarkFood />
-      <Salad />
-      <Drinks />
+      <Products />
     </>
   );
 }
@@ -24,10 +18,10 @@ function Main() {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Register />} />
       <Route path="/admin" element={<Admin />} />
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/login' element={<Login/>}/>
+      <Route path="/main" element={<Main />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
